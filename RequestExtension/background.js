bd6c7ea51d78b3458405f50ea8fddb660fe96e1c
richardwang1134 <richardwang1134@gmail.com>
2018-03-02 09:41:27 +0800

    request

--- RequestExtension/background.js
+++ RequestExtension/background.js
@@ -1,10 +1,23 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
-	function(details) 
-	{	console.log(details);
-		return{cancel: true};
+	//callback
+	function(details){
+		for(var i = 0; i < details.requestHeaders.length; i++){
+			if(details.requestHeaders[i].name==="Referer"){
+				var ref = details.requestHeaders[i].value;
+				var url = details.url;
+				if(!url.match(ref)){
+					console.log("ref:",ref);
+					console.log("url:",url);
+					console.log("------------------------------------------");
+				}
+			}
+		}
+		//return{cancel: true};
 	}
+	//filter
 ,	{	urls: ["<all_urls>"],
 		types: ["script"]
 	}
+	//optional, extra information specification
 , 	["blocking", 'requestHeaders']
 );
