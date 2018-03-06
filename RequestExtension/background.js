8117adeac1f151a561fc86682c4471cb239858b7
RichardW <richardwang1134@gmail.com>
2018-03-06 20:56:32 +0800

    whiteList_OK

--- RequestExtension/background.js
+++ RequestExtension/background.js
@@ -1,20 +1,32 @@
+
+//----INIT----
+var arr = ["www.google.com","www.gstatic.com","apis.google.com"]
+var obj = {whiteList:arr}
+chrome.storage.sync.set(obj);
+
+//----REQUEST----
 chrome.webRequest.onBeforeSendHeaders.addListener(
 	//callback
-	function(details){
+	async (details)=>{
 		for(var i = 0; i < details.requestHeaders.length; i++){
-			if(details.requestHeaders[i].name==="Referer"){
-				var ref = details.requestHeaders[i].value;
-				var url = details.url;
-				refDomain = ref.split("/")[2];
-				urlDomain = url.split("/")[2];
-				if(!urlDomain.match(refDomain)){
-					console.log("ref:",refDomain);
-					console.log("url:",urlDomain);
-					console.log("------------------------------------------");
-				}
+			if(details.requestHeaders[i].name === "Referer"){
+				var ref = details.requestHeaders[i].value.split("/")[2];
+				var url = details.url.split("/")[2];
+				if(!url.match(ref)){
+					var whiteList = await getWhiteList(url);
+					for(var j =0; j < whiteList.length; j++){
+						if(url === whiteList[j]){
+							console.log("url:",url);
+							console.log("--------pass--------");
+							return{cancel:false};
+						}
+					}
+					console.log("url:",url);
+					console.log("--------block--------");
+					return{cancel:true};
+				}	
 			}
 		}
-		return{cancel: true};
 	}
 	//filter
 ,	{	urls: ["<all_urls>"],
@@ -23,3 +35,16 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 	//optional, extra information specification
 , 	["blocking", 'requestHeaders']
 );
+
+//----GET_WHITELIST----
+function getWhiteList(url){
+	return new Promise(
+		(resolve)=>{
+			chrome.storage.sync.get(
+				"whiteList",
+				(item)=>{resolve(item.whiteList)}
+			)
+		}
+	);
+}
+
