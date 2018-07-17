7493abf29c03f1b809104a0ce56f5c3e6084782b
maomao1010 <35062056+maomao1010@users.noreply.github.com>
2018-07-17 16:22:05 +0800

    分頁,AES,2階段認證

--- RequestExtension/background.js
+++ RequestExtension/background.js
@@ -1,59 +1,81 @@
 //----INIT----
-var WL = ["tw.yahoo.com"]; //White List	[ref]
-var BR = [["www.google.com","tw.yahoo.com","21:04:05"],["tw.yahoo.com66666666666666666666","192.168.0.1666666666666666666666666666666666666666666666/example2.js","21:04:05"],["tw.yahoo.com","192.168.0.1/example.js","21:04:05"]] //Block Record	[time,url,ref]
+var WL = []; //White List	[ref]
+var BR = []; //Block Record	[time,url,ref]
 var CS = []; //當前頁面的Cookie Status
 var CR = []; //各網頁的Cookie設定
 var URL = "";//當前頁面url
 
-
-//----BLOCK & CHECK REQUEST----
-/*
-chrome.webRequest.onBeforeSendHeaders.addListener(
-	//callback
+//redirect request, then process blocked request
+chrome.webRequest.onBeforeRequest.addListener(
 	(details)=>{
-		for(var i = 0; i < details.requestHeaders.length; i++){//取得ref
-			if(details.requestHeaders[i].name === "Referer"){
-				var ref = details.requestHeaders[i].value;
-				var url = details.url;
-				var urlDomain = url.split("/")[2];
-				var refDomain = ref.split("/")[2];
-				if(urlDomain.match(refDomain)){
-					console.log("  Same   : ",refDomain,urlDomain);
-					return {cancel:false};
-				}
-				for(var j =0; j < WL.length; j++)
-					if(urlDomain == WL[j]){
-						console.log("WhiteList: ",refDomain,urlDomain);
-						return {cancel:false};
-					}
-				add2BR(refDomain,urlDomain);
-				//procBlocked(url,ref);
-				console.log("  Block  : ",refDomain,urlDomain);
-				return {cancel:true};
+		if(details.url!="http://127.0.0.1:8000/browserRequest1"){
+			var url = details.url;
+			var rid = details.requestId.toString();
+			var tid = details.tabId;
+			if(tid == -1){
+				return;
+			}else{
+				procBlocked(url,rid,tid);
+				//console.log("send request 1 of request " + rid);
+				return {redirectUrl:"http://127.0.0.1:8000/browserRequest1"};
 			}
 		}
 	}
-	//filter
 ,	{	urls: ["<all_urls>"],
 		types: ["script"]
 	}
-	//optional, extra information specification
-, 	["blocking", 'requestHeaders']
+, 	["blocking"]
 );
-*/
-/*
-async function procBlocked(url,ref){
-	var result = await delCookie(ref);
-	console.log(result);
-	$.get(
-		url,
-		(data)=>{
-			console.log("get content(type: string) of");
-			console.log(url);
+
+//add new header -- requestId
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	(details)=>{
+		var rid = details.requestId.toString();
+		var obj = {"name":"requestId","value":rid};
+		details.requestHeaders.push(obj);
+		return {requestHeaders: details.requestHeaders};
+	}
+,	{	urls: ["http://127.0.0.1:8000/browserRequest1"],
+		types: ["script"]	}
+
+, 	["blocking","requestHeaders"]
+);
+
+chrome.tabs.onUpdated.addListener(
+	async function (tabId, props, tab) {
+		if (props.status == "loading" && tab.selected) {
+		var ON_CR = false;
+		var ON_WL = false;
+		CS = [];
+		URL = tab.url.split("/")[2];
+		CR = await getCookieRecord();
+		WL = await getWhiteList();
+		if(CR){
+			for(var i = 0 ; i < CR.length ; i++){
+				if( URL == CR[i]){
+					ON_CR = true;
+				}
+			}
 		}
-	);
-}*/
-//***
+		if(WL){
+			for(var i = 0 ; i < WL.length ; i++){
+				if(URL == WL[i]){
+					ON_WL = true;
+				}
+			}
+		}
+		CS = await getCookie(tab.url);
+		if(!ON_WL){
+			for(var i = 0 ; i < CS.length ; i++){
+				CS[i] = await SetSameSite(tab.url,CS[i],'strict');
+			}
+		}
+		if(ON_CR){
+			var CSCR = await getCS(URL);
+			CS = await CompareCS(tab.url,CSCR,CS);
+		}
+	}
+});
 
 chrome.tabs.onSelectionChanged.addListener(
 	function (tabId,selectinfo) {
@@ -81,7 +103,6 @@ chrome.tabs.onSelectionChanged.addListener(
 			}
 			CS = await getCookie(tab.url);
 			if(!ON_WL){
-				console.log("not in WL");
 				for(var i = 0 ; i < CS.length ; i++){
 					CS[i] = await SetSameSite(tab.url,CS[i],'strict');
 				}
@@ -89,13 +110,64 @@ chrome.tabs.onSelectionChanged.addListener(
 			if(ON_CR){
 				var CSCR = await getCS(URL);
 				CS = await CompareCS(tab.url,CSCR,CS);
-				//console.log(CS);
 			}
 	});
-    //console.info("onSelectChanged");
   });
-//*** 
-  
+
+//process blocked request
+async function procBlocked(url,rid,tid){
+	var ref = await getTabURL(tid);
+	var refDomain = ref.split("/")[2];
+	var urlDomain = url.split("/")[2];
+	if(refDomain.match(urlDomain)){;
+	}else if(inWL(ref)){;
+	}else{
+		add2BR(refDomain,urlDomain);
+		var resulet = await delCookie(ref);
+	}
+	sendRequest(rid,url);
+}
+
+function SetSameSite(url,cookie,sameSite){
+	return new Promise((resolve)=>{
+			chrome.cookies.set(
+			{	
+			 	url:url
+				,name:cookie.name
+				,value:cookie.value
+				,domain:cookie.domain
+				,path:cookie.path
+				,secure:cookie.secure
+				,httpOnly:cookie.httpOnly
+				,sameSite:sameSite
+				,expirationDate:cookie.expirationDate
+				,storeId:cookie.storeId
+			},(cookie)=>{
+			resolve(cookie);
+		})
+	})
+}
+//send request with url that before redirect and request id
+function sendRequest(rid,url){
+	//console.log("send request 2 of request " + rid);
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://127.0.0.1:8000/browserRequest2', false);
+	xhr.setRequestHeader("originalURL",url);
+	xhr.setRequestHeader("requestId",rid);
+	xhr.send(); 
+}
+
+//get url by tabId
+function getTabURL(tid){
+	return new Promise(
+		(resolve)=>{
+			chrome.tabs.get(
+				tid,
+				(item)=>{resolve(item.url)}
+			)
+		}
+	)
+}
 chrome.tabs.onUpdated.addListener(
 	async function (tabId, props, tab) {
 		if (props.status == "loading" && tab.selected) {
@@ -121,7 +193,6 @@ chrome.tabs.onUpdated.addListener(
 		}
 		CS = await getCookie(tab.url);
 		if(!ON_WL){
-			//console.log("not in WL");
 			for(var i = 0 ; i < CS.length ; i++){
 				CS[i] = await SetSameSite(tab.url,CS[i],'strict');
 			}
@@ -129,29 +200,59 @@ chrome.tabs.onUpdated.addListener(
 		if(ON_CR){
 			var CSCR = await getCS(URL);
 			CS = await CompareCS(tab.url,CSCR,CS);
-			//console.log(CS);
 		}
 	}
 });
-
-function add2BR(refDomain,urlDomain){
-	for(var i = 0; i < BR.length; i++){
-		if(refDomain == BR[i][2]){
-			BR[i][2]=getTime();
-			return{cancel:false};
+function CompareCS(url,CSCR,CS){
+	return new Promise(
+	async function(resolve){
+		for(var i = 0 ; i < CS.length ; i++){
+			for(var j = 0 ; j < CSCR.length ; j++){
+				if(CS[i].name == CSCR[j].name){
+					CS[i] = await SetSameSite(url,CS[i],CSCR[j].sameSite);
+				}
+			}
 		}
+		resolve(CS);
 	}
-	if(i >= BR.length)
-		BR.push([getTime(),urlDomain,refDomain]);
+	);
+}
+//confirm wheather domain is in WL
+function inWL(domain){
+	if(WL){
+		for(var i =0; i < WL.length; i++)
+			if(domain == WL[i])	return true;
+	}
+	return false;
 }
 
-//----GET_WHITELIST----
+//add record to block record list
+async function add2BR(refDomain,urlDomain){
+	var found = await FoundInBR(refDomain);
+	if(!found)
+		BR.push([refDomain,urlDomain,getTime()]);
+}
+function FoundInBR(refDomain){
+	return new Promise(
+		(resolve)=>{
+			for(var i = 0 ; i < BR.length ; i++){
+				if(refDomain == BR[i][1]){
+					BR[i][2] = getTime();
+					resolve(false);
+				}
+			}
+			resolve(false);
+		}
+	)
+}
+
+//get white list
 function getWhiteList(){
 	return new Promise(
 		(resolve)=>{
 			chrome.storage.sync.get(
 				"whiteList",
-				(item)=>{resolve(item["whiteList"])}
+				(item)=>{resolve(item.whiteList)}
 			)
 		}
 	);
@@ -177,7 +278,7 @@ function getCS(url){
 	);
 }
 
-//----DELETE COOKIE----
+//delete cookie by url
 function delCookie(url){
 	return new Promise(
 		(resolve,reject)=>{
@@ -203,11 +304,7 @@ function delCookie(url){
 	);
 }
 
-async function printCoockie(url){
-	var c = await getCookie(url);
-	console.log("cccc",c);
-}
-
+//get cookie by url
 function getCookie(url){
 	return new Promise(
 		(resolve)=>{
@@ -222,6 +319,7 @@ function getCookie(url){
 	)
 }
 
+//get time
 function getTime(){
 	var d = new(Date);
 	var h = d.getHours();
@@ -233,7 +331,6 @@ function getTime(){
 	return(h + ":" + m + ":" + s);
 }
 
-/*----SEND MSG OF [WHITELIST & BLOCK RECORD]----*/
 chrome.runtime.onMessage.addListener(
 	(request, sender, sendResponse)=>{
 	  	if (request.get == "wl"){
@@ -296,39 +393,3 @@ chrome.runtime.onMessage.addListener(
 		}	
 	}
 );
-function SetSameSite(url,cookie,sameSite){
-	return new Promise((resolve)=>{
-			chrome.cookies.set(
-			{	
-			 	url:url
-				,name:cookie.name
-				,value:cookie.value
-				,domain:cookie.domain
-				,path:cookie.path
-				,secure:cookie.secure
-				,httpOnly:cookie.httpOnly
-				,sameSite:sameSite
-				,expirationDate:cookie.expirationDate
-				,storeId:cookie.storeId
-			},(cookie)=>{
-			resolve(cookie);
-		})
-	})
-}
-
-function CompareCS(url,CSCR,CS){
-	return new Promise(
-	async function(resolve){
-		for(var i = 0 ; i < CS.length ; i++){
-			for(var j = 0 ; j < CSCR.length ; j++){
-				if(CS[i].name == CSCR[j].name){
-					CS[i] = await SetSameSite(url,CS[i],CSCR[j].sameSite);
-					//console.log(CS[i]);
-				}
-			}
-		}
-		//console.log(CS);
-		resolve(CS);
-	}
-	);
-}
